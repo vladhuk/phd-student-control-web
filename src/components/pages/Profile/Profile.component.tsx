6d4b681258dd30df017b7aa8e1528ed561d5844e
vladhuk <vladhookovskiy@gmail.com>
2021-05-12 01:44:20 +0300

    - Add profile;

--- src/components/pages/Profile/Profile.component.tsx
+++ src/components/pages/Profile/Profile.component.tsx
@@ -1,8 +1,27 @@
+import axios from 'axios';
 import { Sidebar } from 'components/organisms/Sidebar/Sidebar.component';
 import { Topbar } from 'components/organisms/Topbar/Topbar.component';
-import React, { FunctionComponent } from 'react';
+import { API_PATH } from 'constants/index';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 
 export const Profile: FunctionComponent = () => {
+  const [profile, setProfile] = useState({
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${API_PATH}/users/me`, {
+        headers: { Authorization: window.localStorage.getItem('token') },
+      })
+      .then(({ data }) => {
+        setProfile(data);
+      });
+  }, []);
+
   return (
     <div id="wrapper">
       <Sidebar />
@@ -11,7 +30,22 @@ export const Profile: FunctionComponent = () => {
           <Topbar />
           <div className="container-fluid">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
-              <h1 className="h3 mb-0 text-gray-800">Профіль</h1>
+              <h2 className="mb-0 text-gray-800">Профіль</h2>
+            </div>
+            <hr />
+            <div className="row mt-4">
+              <h5>ID:</h5>
+              <div>{profile.id}</div>
+            </div>
+            <div className="row mt-4">
+              <h5>Ім&lsquo;я:</h5>
+              <div>
+                {profile.firstName} {profile.lastName}
+              </div>
+            </div>
+            <div className="row mt-4">
+              <h5>Пошта:</h5>
+              <div>{profile.email}</div>
             </div>
           </div>
         </div>
